a4c09f27b0bfe9bb7b54135ee47507e38789cf88
辻真緒 <>
2019-06-11 09:50:49 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE HTML>
+<!--
+	Story by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+	Note: Only needed for demo purposes. Delete for production sites.
+-->
+<html>
+	<head>
+
+		<title>MaoTsuji profile</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+  	<!-- Favicons -->
+	  <link href="images/icon_taichi.png" rel="icon">
+
+	    <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	</head>
+
+	<body class="is-preload">
+
+		<!--header-->
+		<div id="thenavbar" class="navbar navbar-default navbar-fixed-top">
+	    <div class="container">
+  	    <div class="navbar-collapse collapse">
+    	    <ul class="nav navbar-nav navbar-left">
+      	    <li class="active"><a href="#wrapper" class="smoothscroll">Home</a></li>
+	        	  <li><a href="#career" class="smoothscroll">Career</a></li>
+	          <li><a href="#hobby" class="smoothscroll">Hobby & Skill</a></li>
+  	        <li><a href="#selfcoaching" class="smoothscroll">Self Coaching</a></li>
+    	      <li><a href="#contact" class="smoothscroll">Contact</a></li>
+      	  </ul>
+      	</div>
+      	<!--/.nav-collapse -->
+    	</div>
+  	</div>
+
+  	
+
+
+
+
+		<!-- Wrapper -->
+			<div id="wrapper" class="divided">
+
+				<section id="home" class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
+					<div class="content">
+						<h1>Mao TSUJI</h1>
+							<p>Please call me <strong>MaoT</strong>or <strong>Tsuji chan</strong>.</p>
+					</div>
+					<div class="image">
+						<img src="images/profile_sneak.JPG" alt="" />
+					</div>
+				</section>
+
+
+
+				<!-- Spotlight -->
+					<section id="from" class="spotlight style1 orient-right content-align-left image-position-center onscroll-image-fade-in" id="first">
+						<div class="content">
+							<h2>Amagasaki, Hyogo</h2>
+							<p>I was born and rased in Amagasaki City, Hyogo.<br>
+							After graduation I moved to Kyoto.
+							</p>
+							<ul class="actions stacked">
+								<li><a href="http://www.city.amagasaki.hyogo.jp/" class="button">Learn More</a></li>
+							</ul>
+						</div>
+						<div class="image">
+							<img src="images/amagasaki.jpeg" alt="" />
+						</div>
+					</section>
+
+				<!-- Spotlight -->
+					<section id="career" class="spotlight style1 orient-left content-align-left image-position-center onscroll-image-fade-in">
+						<div class="content">
+							<h2>Mechanical Engineering</h2>
+							<p>I have a <strong>master's degree</strong> in mechanical engineering.<br>
+							I learned Robotics and Bio engineering.<br>
+							My research was <strong>"Analyzinig muscle synergy of human lower limb during walking with high heels. "</strong>
+							</p>
+
+							<p>This major is normally full of men...</p>
+							<ul class="actions stacked">
+								<li><a href="http://robotics.me.es.osaka-u.ac.jp/MiyazakiLab/" class="button">Learn More</a></li>
+							</ul>
+						</div>
+						<div class="image">
+							<img src="images/labo_douki1.jpg" alt="" />
+						</div>
+					</section>
+
+				<!-- Spotlight -->
+					<section id="company" class="spotlight style1 orient-right content-align-left image-position-center onscroll-image-fade-in">
+						<div class="content">
+							<h2>Chemical Company</h2>
+							<p>I've worked at the Chemical Company as a <strong>Production engineer</strong> for 3 years.<br>
+
+							My work was <strong>developing the process of the new production</strong>, TIM (Thermal Interface Material)<br>
+							<br>
+							Also, full of men in my department...
+
+							</p>
+							<ul class="actions stacked">
+								<li><a href="https://www.sekisui.co.jp/" class="button">Learn More</a></li>
+							</ul>
+						</div>
+						<div class="image">
+							<img src="images/kaisya_douki1.JPG" alt="" />
+						</div>
+					</section>
+
+				<!-- Items -->
+					<section id="skill" class="wrapper style1 align-center">
+						<div class="inner">
+							<h2>Programming Skill</h2>
+							<div class="items style1 medium onscroll-fade-in">
+								<section>
+									<div class="iconimg">
+										<img src="images/icon_c.png" width="30">
+									</div>
+									<h3>C language</h3>
+									 <p><b>(Beginner)</b><br>
+									be able to calculation simple numerical fomula.
+									</p>
+								</section>
+
+								<section>
+									<div class="iconimg">
+										<img src="images/icon_python.png" width="30">
+									</div>
+									<h3>Python</h3>
+									<p><b>(Beginner)</b><br>
+									be able to calculation simple numerical fomula.
+									</p>
+								</section>
+							</div>
+						</div>
+					</section>
+
+					<!-- Hobby Skills -->
+					<section class="wrapper style1 align-center" id="hobby">
+						<div class="inner">
+							<h2>Hobby & Skills</h2>
+							<p><strong>Kung-fu(WUSHU)</strong>, Yoga,<strong>ultimate frisbee</strong>, <strong>Traveling abroad</strong>, etc.</p>
+							<div class="index align-left">
+							</div>
+						</div>
+						<!-- Gallery -->
+							<div class="gallery style2 medium lightbox onscroll-fade-in">
+								<article>
+									<a href="images/wushu_soukou.jpg" class="image">
+										<img src="images/wushu_soukou.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>Kung-fu Competition</h3>
+										<p>Double hook swords.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+								<article>
+									<a href="images/wushu_medal.jpg" class="image">
+										<img src="images/wushu_medal.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>WUSHU AWARD</h3>
+										<p>I got medals of Zachuen and Double hook swords in Osaka.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+								<article>
+									<a href="images/gaia2018.jpg" class="image">
+										<img src="images/gaia2018.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>Ultimate frisbee</h3>
+										<p>Gaia ultimate competition near Mt.Fuji.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+								<article>
+									<a href="images/machupichu1.jpg" class="image">
+										<img src="images/machupichu1.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>Machupichu in Peru</h3>
+										<p>One of the most favorite place.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+								<article>
+									<a href="images/rainbow_mountain.jpg" class="image">
+										<img src="images/rainbow_mountain.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>Mt.Rinbow in Peru</h3>
+										<p>One of the most favorite place.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+								<article>
+									<a href="images/tagaytay.jpg" class="image">
+										<img src="images/tagaytay.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>Tagaytay in the Phillippines</h3>
+										<p>Jeepney charter.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+								<article>
+									<a href="images/musclebar.jpg" class="image">
+										<img src="images/musclebar.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>Muscle Bar</h3>
+										<p>Maccho are squeezing Pinapple sour at Maucle bar in Kitashinchi.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+								<article>
+									<a href="images/tnnk.jpg" class="image">
+										<img src="images/tnnk.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>T.M.Revokution<br>
+										(Nishikawa Takanori)</h3>
+										<p>I often sing Hot Limit in Karaoke.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+							</div>
+					</section>
+					<!--255 168-->
+
+				<!-- Selfcoaching -->
+					<section id="selfcoaching" class="wrapper style1 align-center">
+						<div class="inner">
+							<h2>Self Coaching</h2>
+							<p>A Step-By-Step Process for Self-Coaching Myself Productivity (2019/04/06 now).</p>
+							<div class="index align-left">
+								<!-- Ordered List -->
+									<section>
+										<header>
+											<h3>Best 5</h3>
+										</header>
+										<div class="content">
+											<ol>
+												<li>More than 850 scores on the TOEIIC (after 1 year)</li>
+             						<li>Working in a venture company (2020)</li>
+             						<li>Acquire knowledge for Business (Every day)</li>
+					              <li>Win a medal at the all Japan Wushu Championship (2020)</li>
+          					    <li>Bigger income than my ex-colleagues (2022)</li>
+											</ol>
+										</div>
+									</section>
+							</div>
+						</div>
+					</section>
+
+					<section id="contact" class="wrapper style1 align-center">
+						<div class="inner">
+							<h2>Contact</h2>
+							<p>Please feel free to contact me!</p>
+							<ul class="icons">
+								<li><a href="https://www.facebook.com/tsuji.mao" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+								<li><a href="https://www.instagram.com/maomao2g/?hl=ja" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+								<li><a href="#" class="icon style2 fa-envelope"><span class="label">Email</span></a></li>
+							</ul>
+
+						</div>
+					</section>
+
+
+				<!-- Footer -->
+					<footer id="footer" class="wrapper style1 align-center">
+						<div class="inner">
+							
+							
+							<p>©MaoTsuji profile(2019)</p>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+		<!-- Note: Only needed for demo purposes. Delete for production sites. -->
+			<script src="assets/js/demo.js"></script>
+
+	</body>
+</html>
